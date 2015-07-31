a6f35856e3c1c1297260c119016c380c9ed53773
root <root@malina.webhop.me>
2015-07-31 23:16:17 +0200

    Imported patterns library in urls.py

--- webAPI/webAPI/urls.py
+++ webAPI/webAPI/urls.py
@@ -1,19 +1,19 @@
-from django.conf.urls import url, include
+from django.conf.urls import url, include, patterns
 from rest_framework import routers
 from client import views
 
 router = routers.DefaultRouter()
-router.register(r'rest/users', views.UserViewSet)
-router.register(r'rest/groups', views.GroupViewSet)
+#router.register(r'rest/users', views.UserViewSet)
+#router.register(r'rest/groups', views.GroupViewSet)
 # router.register(r'rest/update/(?P<pk>[0-9]+)', views.GPIOUpdateView)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    url(r'^', include(router.urls)),
+urlpatterns = patterns('',
+    #url(r'^', include(router.urls)),
     url(r'^rest/', include('rest_framework.urls', namespace='rest_framework')),
     #url(r'^rest/update/$', views.GPIOUpdateView.as_view(), name='detail'),
     url(r'^rest/gpio/all/', views.GPIOListView.as_view(), name='item_statuses'),
-    url(r'^rest/gpio/update/(?P<pk>[0-9]+)/', views.GPIODetailView.as_view(), name='item_update')
+    url(r'^rest/gpio/update/(?P<pk>[0-9]+)/', views.GPIODetailView.as_view(), name='item_update'),
 
-]
+)
